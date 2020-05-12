f126e31f38af75fa98b45216ebfeefc4505a1071
seetharamulu <thumurugoti.seetharamulu@gmail.com>
2020-05-12 21:20:53 +0530

    added employeewage

--- src/com/Empwage/test/EmployeeMain.java
+++ src/com/Empwage/test/EmployeeMain.java
@@ -4,27 +4,39 @@ import com.Empwage.model.Employee;
 import com.Empwage.controller.Employeecontroller;
 public class EmployeeMain{
 
+
 	 public static void main(String[] args){
 
-		Employee employee = new Employee();
-		Employeecontroller employeecontroller = new Employeecontroller();
-		EmployeeMain employeeMain = new EmployeeMain();
+		int status=0;
+		int workhours=0;
+		int dailywage=0;
+		int totalwage=0;
 
-		int status=employeecontroller.EmployeeStatus();
-		switch (status){
-			case 0: 
-	                        System.out.println("absent");
-				break;
-			case 1:
-				System.out.println("present");
-				int workhours=employeecontroller.Employee_Workhours();
-				switch (workhours){
-					case 8:
-						System.out.println("fulltime wage of emplloyee is : "+ employeecontroller.Employeewage(workhours));
-						break;
-					default:
-						System.out.println("parttime wage of emplloyee is : "+ employeecontroller.Employeewage(workhours));
-				}
-		}
+		for(int days=0; days<20; days++){
+
+			Employee employee = new Employee();
+			Employeecontroller employeecontroller = new Employeecontroller();
+			EmployeeMain employeeMain = new EmployeeMain();
+
+			status=employeecontroller.EmployeeStatus();
+			switch (status){
+				case 0: 
+	                        	//System.out.println("absent");
+					break;
+				case 1:
+					//System.out.println("present");
+					workhours=employeecontroller.Employee_Workhours();
+					/*switch (workhours){
+						case 8:
+							System.out.println("fulltime wage of emplloyee is : "+ employeecontroller.Employeewage(workhours));
+							break;
+						default:
+							System.out.println("parttime wage of emplloyee is : "+ employeecontroller.Employeewage(workhours));
+					}*/
+					totalwage+=employeecontroller.Employeewage(workhours);
+			}
+	}
+	 System.out.println("monthly wage is : "+ totalwage);
+
 	}
 }
